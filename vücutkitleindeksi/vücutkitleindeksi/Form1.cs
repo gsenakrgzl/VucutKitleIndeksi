f4ce8c79fd73aea0bff025c19c713a3fdc45f56b
gsenakrgzl <karaguzelsena46@gmail.com>
2024-02-22 17:37:54 +0300

    Vücut Kitle İndeksi
    C# ile geliştirilmiştir.

---
+++ vücutkitleindeksi/vücutkitleindeksi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vücutkitleindeksi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double boy, kilo, vki;
+            boy = Convert.ToDouble(textBox1.Text);
+            kilo = Convert.ToDouble(textBox2.Text);
+            vki = kilo / (boy * boy);
+            label4.Text = vki.ToString();
+            if(vki < 18)
+            {
+                label6.Text = "Zayıf";
+            }
+            else if (vki >= 18 && vki < 25)
+            {
+                label6.Text = "Normal";
+            }
+            else if (vki >= 25 && vki < 30)
+            {
+                label6.Text = "Hafif Kilolu";
+            }
+            else
+            {
+                label6.Text = "Obez";
+            }
+        }
+    }
+}
